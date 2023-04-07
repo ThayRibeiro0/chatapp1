df43b036d0a9a258c3cef4323d5a55c336369dd5
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2023-04-07 16:03:19 -0700

    6

--- src/components/SidebarHeader/styles.js
+++ src/components/SidebarHeader/styles.js
@@ -1,31 +1,30 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 59px;
-    background-color: #f0faf5;
-    padding: 10px 16px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottn: 5px;
-    box-shadow: 0 1px 2px #0003;
+  height: 59px;
+  background-color: #f0f2f5;
+  padding: 10px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  box-shadow: 0 1px 2px #0003;
 `;
 
 export const Avatar = styled.img`
-    width: 35px;
-    height: 35px;
-    cursor: pointer;
-    border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  border-radius: 50%;
 `;
 
 export const Options = styled.div`
-    display: flex;
-    gap: 10px
-
-    svg {
-        width: 24px;
-        height: 24px;
-        color: #5465ef;
-        cursor: pointer;
-    }
+  display: flex;
+  gap: 10px;
+  svg {
+    width: 24px;
+    height: 24px;
+    color: #54656f;
+    cursor: pointer;
+  }
 `;
